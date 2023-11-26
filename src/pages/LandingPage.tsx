8bd447fb29fb0d5f527f2e9d1fb9cf6ed1baa8af
Veronica Corvalan <veroo.corvalan@gmail.com>
2023-11-25 21:44:54 -0300

    commit

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,13 +1,40 @@
+import { useEffect, useState } from "react";
 import CarouselHome from "../components/CarouselHome/CarouselHome"
 import CategoriasSelector from "../components/CategoriasSelector/CategoriasSelector"
 import CategoriasTareas from "../components/CategoriasTareas/CategoriasTareas"
+import { Task } from "../types/task";
+import { TaskService } from "../services/TaskService";
 
 const LandingPage = () => {
+
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [filteredTasks, setFilteredTasks] = useState<Task[]>([]);
+    const [selectedCategory, setSelectedCategory] = useState<string>('');
+
+    useEffect(() => {
+        const fetchTasks = async () => {
+            const tasksData = await TaskService.getAllTasks();
+            setTasks(tasksData);
+        };
+
+
+        fetchTasks();
+    }, []);
+
+    useEffect(() => {
+        if (selectedCategory) {
+            const filtered = tasks.filter(task => task.estado.toUpperCase() === selectedCategory.toUpperCase());
+            setFilteredTasks(filtered);
+        } else {
+            setFilteredTasks(tasks);
+        }
+    }, [selectedCategory, tasks]);
+
     return (
         <>
             <CarouselHome />
-            <CategoriasSelector />
-            <CategoriasTareas />
+            <CategoriasSelector onSelectedCategory={setSelectedCategory} />
+            <CategoriasTareas task={filteredTasks.length > 0 ? filteredTasks : tasks} />
         </>
     )
 }
